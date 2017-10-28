8a22a47351056f2dea445eb98a857a717783422f
Mukazuge <mukazuge@gmail.com>
2017-10-28 21:02:39 +0000

    [SERVICE] adding new subject to handle speech creation

--- src/app/common/app-state.service.ts
+++ src/app/common/app-state.service.ts
@@ -4,9 +4,15 @@ import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 @Injectable()
 export class AppStateService {
   public subject = new BehaviorSubject<any>(0);
+  public editSpeechSubject = new BehaviorSubject<any>(0);
+  public editSpeechEvent = this.editSpeechSubject.asObservable();
   public event = this.subject.asObservable();
 
   public publishState(data: any) {
     this.subject.next(data);
+  }
+
+  public publishEditSpeech(data: boolean) {
+    this.editSpeechSubject.next(data);
   }
 }
