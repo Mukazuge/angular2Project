f057c7f62ca7e51f8d731eb79d35ff66397b0099
Mukazuge <mukazuge@gmail.com>
2017-10-30 00:27:46 +0000

    [MODULE] adding new ng2shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome';
+import { ShareModule } from 'ng2share/share.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
-import { AppStateService } from './common/app-state.service';
 import { PageNotFoundComponent } from './page_not_found/page-not-found.component';
 import { SearchComponent } from './common/search/search.component';
 import { SpeechListComponent } from './common/speech-list/speech-list.component';
 import { EditSpeechComponent } from './common/speech-edit/speech-edit.component';
-import { SpeechService } from './common/speech.service';
-import { HttpModule } from '@angular/http';
-import { Angular2FontawesomeModule } from 'angular2-fontawesome';
 import { WarningModalComponent } from './common/warning-modal.component/warning-modal.component';
+
+import { AppStateService } from './common/app-state.service';
+import { SpeechService } from './common/speech.service';
 
 import { EscapeHtmlPipe } from './common/escape-html.pipe';
 
@@ -26,6 +29,8 @@ import { EscapeHtmlPipe } from './common/escape-html.pipe';
     FormsModule,
     HttpModule,
     Angular2FontawesomeModule,
+    ShareModule,
+    BrowserAnimationsModule,
     NgbModule.forRoot()
   ],
   declarations: [
