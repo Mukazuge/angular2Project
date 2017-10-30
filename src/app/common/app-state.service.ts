8c610db199126fbe885784e29db624db620e1508
Mukazuge <mukazuge@gmail.com>
2017-10-30 07:35:55 +0000

    [STYLE] deleting border and box shadow from btn-link

--- src/app/common/app-state.service.ts
+++ src/app/common/app-state.service.ts
@@ -3,13 +3,11 @@ import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class AppStateService {
-  public subject = new BehaviorSubject<any>(0);
+  public bulkSpeechesSubject = new BehaviorSubject<any>(0);
   public editSpeechSubject = new BehaviorSubject<any>(0);
-  public editSpeechEvent = this.editSpeechSubject.asObservable();
-  public event = this.subject.asObservable();
 
   public publishState(data: any) {
-    this.subject.next(data);
+    this.bulkSpeechesSubject.next(data);
   }
 
   public publishEditSpeech(data: any) {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,7 +21,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     // notify when a api request is success
-    this.subscription = this.appState.event.subscribe((res) => {
+    this.subscription = this.appState.bulkSpeechesSubject.subscribe((res) => {
       if (Array.isArray(res)) {
         this.speeches = res;
       } else {
